2e991459f496c2bdfb6c0b1d344d49da55c1722e
Peter Wang <happypeter1983@gmail.com>
2017-03-22 21:38:45 +0800

    react-webpack dev env

---
+++ client/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
