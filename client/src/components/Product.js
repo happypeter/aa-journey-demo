d2f13912ed133d3752976dd577d447bf7174271a
Peter Wang <happypeter1983@gmail.com>
2017-03-29 11:31:32 +0800

    new-product component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -10,18 +10,68 @@ class Product extends React.Component {
     }
   }
 
+  _getCats() {
+    axios.get(`${config.host}/cats`)
+      .then((res)=>{
+        console.log(res.data.cats);
+        this.setState({cats:res.data.cats})
+      })
+      .catch(err=>console.log(err))
+  }
+
   componentWillMount(){
+    this._getCats();
   }
   _handleSubmit(e) {
-
+    e.preventDefault();
+    console.log(this.refs.catId.value);
+    let product = {
+                name: this.refs.name.value,
+                summary: this.refs.summary.value,
+                price: this.refs.price.value,
+                poster: this.refs.poster.value,
+                cat: this.refs.catId.value
+            };
+    console.log(product);
+    axios.post(`${config.host}/product/new`, product)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
   }
   render(){
+    let optionList = this.state.cats.map(item => {
+        return(
+          <option value={item._id} key={Math.random()}>
+            {item.name}
+          </option>)
+      });
     return (
         <div className="product">
           <span>新建商品</span>
           <form onSubmit={this._handleSubmit.bind(this)}>
-            <input ref='productName' type="text" />
-            <input type='submit' />
+            <p>
+            name
+            <input ref='name' type="text" />
+            </p>
+            <p>
+            summery
+            <input ref='summary' type="text" />
+            </p>
+            <p>
+            price:
+            <input ref='price' type="text" />
+            </p>
+            <p>
+            poster:
+            <input ref='poster' type="text" />
+            </p>
+            <p>
+            <select name="catId" ref="catId">
+              { optionList }
+            </select>
+            </p>
+            <p>
+              <input type='submit' />
+            </p>
           </form>
         </div>
     )
