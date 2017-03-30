a5e2494f37b1bcbb79aaca44090d6a730a077799
Peter Wang <happypeter1983@gmail.com>
2017-03-30 21:40:27 +0800

    API: user/signin/logout/signup

---
+++ api/controllers/user.js
@@ -0,0 +1,50 @@
+let User = require('../models/user.js');
+// 注册
+exports.signup = function (req, res) {
+  let _user = req.body;
+  User.findOne({username:_user.username},function (err,user) {
+    if (err) return res.status(500).json({msg: '注册失败，请重试',err});
+    if (user) {
+      return res.status(403).json({msg: '用户名重复，请重新注册'})
+    }else {
+      // 这里不适用 var 声明是因为函数的形参有了这个变量，为 null
+      user = new User(_user);
+      user.save(function (err,user) {
+        if (err) return res.status(500).json({msg: '注册失败，请重试',err});
+        res.json({
+          userId: user._id,
+          username: user.username,
+          msg: '注册成功'
+        })
+      })
+    }
+  })
+}
+
+
+// 登陆
+exports.signin = function (req, res) {
+  var _user = req.body;
+  User.findOne({username:_user.username},function (err,user) {
+    if (err) return res.status(500).json({msg: '登陆失败，请重试',err});
+    if (!user) {
+      return res.status(401).json({msg: '登陆失败，用户名不存在'})
+    }
+    user.comparePassword(_user.password, function (err, isMatch) {
+      if (err) return res.status(500).json({msg: '登陆失败，请重试',err});
+      if (isMatch) {
+        res.json({
+          userId: user._id,
+          user: user.username,
+          msg: '登陆成功'
+        })
+      }else {
+        res.status(401).json({msg: '密码错误，请核对后重试'})
+      }
+    })
+  })
+}
+// 登出功能
+exports.logout = function (req,res) {
+  res.json({ msg: '登出成功' })
+}
--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,7 @@
 let Cat = require('./controllers/cat');
 let Product = require('./controllers/product');
+let User = require('./controllers/user');
+
 module.exports = function (app) {
   app.post('/cat',Cat.add)
   app.get('/cats',Cat.list)
@@ -10,4 +12,7 @@ module.exports = function (app) {
   app.get('/product/detail/:id',Product.detail)
   app.delete('/product/delete/:id',Product.del)
 
+  app.post('/user/signup', User.signup)
+  app.post('/user/signin', User.signin)
+  app.get('/user/logout', User.logout)
 }
