6a76f3204e4eae75eacff57a7a60aecb426a4e56
Peter Wang <happypeter1983@gmail.com>
2017-03-31 21:30:10 +0800

    singup react

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,6 +9,8 @@ class Header extends React.Component {
         <Link to='/'>Home</Link>
         <Link to='/new-cat'>新建分类</Link>
         <Link to='/new-product'>新建商品</Link>
+        <Link to='/signin'>登录</Link>
+        <Link to='/signup'>注册</Link>
       </div>
     )
   }
--- client/src/router.js
+++ client/src/router.js
@@ -5,6 +5,8 @@ import App from './components/App.js'
 import Home from './components/Home.js'
 import NewCat from './components/NewCat.js'
 import Product from './components/Product.js'
+import SignIn from './components/SignIn.js'
+import SignUp from './components/SignUp.js'
 
 export default function () {
   return(
@@ -13,6 +15,8 @@ export default function () {
         <IndexRoute component={Home}/>
         <Route path='/new-cat' component={NewCat} />
         <Route path='new-product' component={Product} />
+        <Route path='signin' component={SignIn} />
+        <Route path='signup' component={SignUp} />
       </Route>
     </Router>
   )
